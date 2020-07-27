1f83901270e84b0fff78587f1a78cc82aa5715a5
Admin <Admin@Admin->
2020-07-27 23:10:52 +0300

    27.07.20 15 tasks

--- demo/src/com/sbrf/demo/Startup.java
+++ demo/src/com/sbrf/demo/Startup.java
@@ -182,5 +182,31 @@ public class Startup {
                         }
                 }
  */
+/* Задача 15
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите значение a: ");
+                double a = scanner.nextDouble();
+                System.out.print("Введите значение b: ");
+                double b = scanner.nextDouble();
+                System.out.print("Введите значение c: ");
+                double c = scanner.nextDouble();
+                double d=(b*b)-(4*a*c);
+                double e=0;
+                double f=0;
+                if(d==0){
+                        e=-1*(b/(2*a));
+                        System.out.println("Уравнеие имеет 1 квадратный корень, который равен "+e);
+                }
+                else {
+                        if(d>0){
+                                e=((-1*b)+(double)Math.sqrt(d))/(2*a);
+                                f=((-1*b)-(double)Math.sqrt(d))/(2*a);
+                                System.out.println("Уравнение имеет 2 квадратных корня: "+e+" и " +f);
+                        }
+                        else {
+                                System.out.println("Уравнение не имеет квадратных корней");
+                        }
+                }
+ */
         }
 }
