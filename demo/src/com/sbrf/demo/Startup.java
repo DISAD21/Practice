4454d4851cf8d46c5da8cb1f729e4589c7662cc6
Admin <Admin@Admin->
2020-07-27 14:12:20 +0300

    20.07.20 3 tasks

--- demo/src/com/sbrf/demo/Startup.java
+++ demo/src/com/sbrf/demo/Startup.java
@@ -5,21 +5,27 @@ public class Startup {
         public static void main(String[] args) {
 /* 1 задача
             System.out.println("Hello World!");
- */
-            /* 2 задача
+*/
+/* 2 задача
             int a = 21;
             int b = 2;
             int c = a/b;
             int d = a%b;
             System.out.println(a + " / " + b + " = " + c + " и " + d + " в остатке");
-                         */
+*/
+/* 3 задача
+     boolean weekday = false;
+     boolean vacation = true;
+     boolean Sleep;
+        if(!weekday && vacation) {
+            Sleep = true;
+        } else
+            Sleep = false;
+            System.out.println("Спать = " + Sleep);
+        }
+*/
+            
 
-     boolean weekday = true;
-     boolean vacation = false;
-        if(!weekday || vacation) {
-            return true;
-        }
-        return false;
-        }
+
 
 }
