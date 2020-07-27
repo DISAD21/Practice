c43c2598f1df8df5a957cb16be5e6619c02b18cd
Admin <Admin@Admin->
2020-07-27 14:01:39 +0300

    20.07.20

---
+++ demo/src/com/sbrf/demo/Startup.java
@@ -0,0 +1,25 @@
+package com.sbrf.demo;
+
+public class Startup {
+
+        public static void main(String[] args) {
+/* 1 задача
+            System.out.println("Hello World!");
+ */
+            /* 2 задача
+            int a = 21;
+            int b = 2;
+            int c = a/b;
+            int d = a%b;
+            System.out.println(a + " / " + b + " = " + c + " и " + d + " в остатке");
+                         */
+
+     boolean weekday = true;
+     boolean vacation = false;
+        if(!weekday || vacation) {
+            return true;
+        }
+        return false;
+        }
+
+}
