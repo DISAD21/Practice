29b0fc2fc9da1cb8a5988403d372df5df419b959
Admin <Admin@Admin->
2020-07-28 15:02:39 +0300

    28.07.20 Practice 2 task 2

---
+++ demo/src/com/sbrf/practice_day2/Main.java
@@ -0,0 +1,69 @@
+package com.sbrf.practice_day2;
+
+class Vector {
+    // Три приватных поля
+    private double x;
+    private double y;
+    private double z;
+    // С тремя параметрами
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    // Длина вектора. Корень из суммы квадратов
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+    // метод, вычисляющий скалярное произведение
+    public double scalarProduct(Vector vector) {
+        return x * vector.x + y * vector.y + z * vector.z;
+    }
+    // метод, вычисляющий векторное произведение
+    public Vector crossProduct(Vector vector) {
+        return new Vector(
+                y * vector.z - z * vector.y,
+                z * vector.x - x * vector.z,
+                x * vector.y - y * vector.x);
+    }
+    // метод, вычисляющий векторное сложение
+    public Vector crossAddition(Vector vector) {
+        return new Vector(
+                x + vector.x,
+                y + vector.y,
+                z + vector.z);
+    }
+    // метод, вычисляющий векторное вычитание
+    public Vector crossSubtraction(Vector vector) {
+        return new Vector(
+                x - vector.x,
+                y - vector.y,
+                z - vector.z);
+    }
+    public static Vector[] generate(int n){
+        Vector[] vectors = new Vector[n];
+        for(int i =0; i < n; i++){
+            vectors[i] = new Vector(Math.random(), Math.random(), Math.random());
+        }
+        return vectors;
+    }
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+public static void main(String[] args) {
+        Vector[] vectors = Vector.generate(5);
+        System.out.println(vectors[0]);
+        System.out.println(vectors[1]);
+        System.out.println(vectors[0].length());
+        System.out.println(vectors[0].scalarProduct(vectors[1]));
+        System.out.println(vectors[0].crossAddition(vectors[1]));
+        System.out.println(vectors[0].crossSubtraction(vectors[1]));
+        System.out.println(vectors[0].crossProduct(vectors[1]));
+        }
+}
+
